7be64aef788612a0aa3db7af1cacde0a8555e56d
Israh <108226107+Israh-Abdul@users.noreply.github.com>
2025-04-05 19:00:50 +0530

    Update capsule_layers.py

--- capsule_layers.py
+++ capsule_layers.py
@@ -1,68 +1,67 @@
 import tensorflow as tf
-from tensorflow.keras import layers
-import numpy as np
+from tensorflow.keras import layers, initializers, backend as K
 
-@tf.keras.utils.register_keras_serializable()
-class CapsuleLayer(tf.keras.layers.Layer):
-    def __init__(self, num_capsule, dim_capsule, routings=3, **kwargs):
+def squash(vectors, axis=-1):
+    s_squared_norm = K.sum(K.square(vectors), axis, keepdims=True)
+    scale = s_squared_norm / (1 + s_squared_norm) / K.sqrt(s_squared_norm + K.epsilon())
+    return scale * vectors
+
+class CapsuleLayer(layers.Layer):
+    def __init__(self, num_capsule, dim_capsule, routings=3, kernel_initializer='glorot_uniform', **kwargs):
         super(CapsuleLayer, self).__init__(**kwargs)
         self.num_capsule = num_capsule
         self.dim_capsule = dim_capsule
         self.routings = routings
-        self.kernel_initializer = tf.keras.initializers.get('glorot_uniform')
+        self.kernel_initializer = initializers.get(kernel_initializer)
 
     def build(self, input_shape):
-        assert len(input_shape) >= 3, "Input Tensor shape should be [None, input_num_capsule, input_dim_capsule]"
-
         self.input_num_capsule = input_shape[1]
         self.input_dim_capsule = input_shape[2]
-
-        # Transformation matrix
-        self.W = self.add_weight(shape=[self.input_num_capsule, self.num_capsule,
-                                        self.input_dim_capsule, self.dim_capsule],
+        self.W = self.add_weight(shape=[self.num_capsule, self.input_num_capsule,
+                                        self.dim_capsule, self.input_dim_capsule],
                                  initializer=self.kernel_initializer,
-                                 name='W',
-                                 trainable=True)
-        super(CapsuleLayer, self).build(input_shape)
+                                 name='W')
+        super().build(input_shape)
 
     def call(self, inputs, training=None):
-        # Expand dims to perform batch matrix multiplication
-        inputs_expand = tf.expand_dims(inputs, 2)
-        inputs_tiled = tf.expand_dims(inputs_expand, 3)
+        inputs_expand = tf.expand_dims(inputs, 1)
+        inputs_tiled  = tf.tile(inputs_expand, [1, self.num_capsule, 1, 1])
+        inputs_tiled  = tf.expand_dims(inputs_tiled, 4)
+        inputs_hat = tf.map_fn(lambda x: tf.matmul(self.W, x), elems=inputs_tiled)
 
-        # Compute "prediction vectors" by applying transformation matrix W
-        u_hat = tf.matmul(inputs_tiled, self.W)  # shape: [batch_size, input_caps, num_caps, 1, dim_caps]
-        u_hat = tf.squeeze(u_hat, axis=-2)
-
-        # Routing algorithm
-        b = tf.zeros(shape=[tf.shape(inputs)[0], self.input_num_capsule, self.num_capsule])
+        b = tf.zeros(shape=[tf.shape(inputs_hat)[0], self.num_capsule, self.input_num_capsule, 1, 1])
 
         for i in range(self.routings):
-            c = tf.nn.softmax(b, axis=2)
-            c = tf.expand_dims(c, axis=-1)
-            s = tf.reduce_sum(c * u_hat, axis=1)  # weighted sum
-            v = self.squash(s)
-
+            c = tf.nn.softmax(b, axis=1)
+            outputs = tf.reduce_sum(c * inputs_hat, axis=2, keepdims=True)
+            outputs = squash(outputs, axis=-2)
             if i < self.routings - 1:
-                v_expand = tf.expand_dims(v, axis=1)
-                b += tf.reduce_sum(u_hat * v_expand, axis=-1)
+                outputs_tiled = tf.tile(outputs, [1, 1, self.input_num_capsule, 1, 1])
+                agreement = tf.matmul(inputs_hat, outputs_tiled, transpose_a=True)
+                b += agreement
 
-        return v
+        outputs = tf.squeeze(outputs, [2, 4])
+        return outputs
 
-    def squash(self, s, axis=-1):
-        s_norm = tf.norm(s, axis=axis, keepdims=True)
-        scale = (s_norm**2) / (1 + s_norm**2) / (s_norm + tf.keras.backend.epsilon())
-        return scale * s
+    def compute_output_shape(self, input_shape):
+        return (None, self.num_capsule, self.dim_capsule)
 
     def get_config(self):
         config = super(CapsuleLayer, self).get_config()
         config.update({
             'num_capsule': self.num_capsule,
             'dim_capsule': self.dim_capsule,
-            'routings': self.routings
+            'routings': self.routings,
+            'kernel_initializer': tf.keras.initializers.serialize(self.kernel_initializer),
         })
         return config
 
     @classmethod
     def from_config(cls, config):
         return cls(**config)
+
+def PrimaryCap(inputs, dim_capsule, n_channels, kernel_size, strides, padding):
+    output = layers.Conv2D(filters=dim_capsule * n_channels, kernel_size=kernel_size,
+                           strides=strides, padding=padding, name='primarycap_conv2d')(inputs)
+    outputs = layers.Reshape(target_shape=[-1, dim_capsule], name='primarycap_reshape')(output)
+    return layers.Lambda(squash, name='primarycap_squash')(outputs)
